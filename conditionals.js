42260e240dcb7be50444a851bd239a943c9c33dd
Miguel <m.bacho104@gmail.com>
2022-08-18 18:00:40 -0700

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don;t forget your umbrella");
+} 
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're read to go outside!")
+
+node conditionals.js
+
